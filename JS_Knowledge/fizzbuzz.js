5f345bb73d22e63d0c03e69503450b87a1482d7a
Jacob Andersen <jacobma87@gmail.com>
2021-03-01 21:57:02 -0500

    Comments added to fizzbuzz challenge

--- JS_Knowledge/fizzbuzz.js
+++ JS_Knowledge/fizzbuzz.js
@@ -1,3 +1,8 @@
+// A classic coding challenge
+// If a number in a range divides into 3 OR 5 OR BOTH
+// the number is replaced by either Fizz, Buzz or FizzBuzz
+// otherwise, the number is printed
+
 for (i=1;i<=100;i++)
 {
     if (i%3==0&&i%5!=0) {
