42947e673e25dc121b3a6a253ac90d205cb9caa1
Jacob Andersen <jacobma87@gmail.com>
2021-03-10 22:36:09 -0500

    comments added to symmetrical number program

--- Java_Knowledge/Panagram.java
+++ Java_Knowledge/Panagram.java
@@ -1,3 +1,5 @@
+
+
 public class Panagram {
 
     public static void main(String[] args) {
--- Java_Knowledge/SymmetricalNumber.java
+++ Java_Knowledge/SymmetricalNumber.java
@@ -1,8 +1,11 @@
+// program that determines whether a number is symmetric or asymmetric
+// example of a symmetric number : 1221
+// example of an asymmetric number 1234
 public class SymmetricalNumber {
     public static void main(String[] args) {
         
     
-    String number = "9239";
+    String number = "1234";
     boolean isSymmetric = true;
 
     for (int i=0;i<number.length()&&isSymmetric==true;i++) {
